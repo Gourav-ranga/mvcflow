499a240c40c496c20151ebb8c6e5d220dd981069
Gourav-ranga <gouravranga11111@gmail.com>
2019-12-31 12:45:12 +0530

    Updation

---
+++ src/main/java/com/journaldev/spring/model/User.java
@@ -0,0 +1,38 @@
+package com.journaldev.spring.model;
+
+public class User
+{
+	private String id;
+	private String fname;
+	private String lname;
+	private String age;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	
+}
